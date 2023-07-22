f0f57961cfd56275f92482eddee73712a93f8eb8
borsuczyna <65825775+borsuczyna@users.noreply.github.com>
2023-07-22 20:02:10 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,5 +1,5 @@
 for k,v in pairs(getResources()) do
-    if getResourceState(v) == "running" then
+    if getResourceState(v) == "running" and getResourceName(v) ~= "admin" then
         stopResource(v)
         refreshResources()
         setTimer(deleteResource, 1000, 1, v)
