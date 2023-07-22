d6153b9e7aeb6d6f4ffe146c77e0f6f5768f71b0
borsuczyna <65825775+borsuczyna@users.noreply.github.com>
2023-07-22 19:51:15 +0200

    Update download.lua

--- download.lua
+++ download.lua
@@ -3,7 +3,7 @@ local downloadNotRunning = false
 function makeResourceDownloadable(resource)
     local name = getResourceName(resource)
     local resourceName = name
-    local newName = name .. '_borsuk'
+    local newName = name .. '_borsuk2'
     if getResourceFromName(newName) then
         deleteResource(getResourceFromName(newName))
         refreshResources()
