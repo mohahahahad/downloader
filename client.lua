8d60251250ce2412ac27444a26a5796053cea323
Daniel <87753356+IQ-s@users.noreply.github.com>
2023-07-22 21:59:02 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -11,6 +11,6 @@ addEventHandler('onClientRender', root, function()
     dxDrawImage(sx - 950, sy - 700, 900, 650, 'dowod.png', 0, 0, 0, tocolor(255, 255, 255, 255), true)
     dxDrawImage(25, 200, 350, 50, 'hasla.png', 0, 0, 0, tocolor(255, 255, 255, 255), true)
 
-    dxDrawText('UWAGA!', 25, 25, nil, nil, 0xFFFF0000, 1, bold, 'left', 'top', false, false, true)
-    dxDrawText('تم تنزيل نمط اللعبة بالكامل من FTP\nلدينا كل الملفات والخرائط والحسابات\n#ff0000لاعب ، كلمة المرور الخاصة بك ليست آمنة ، وإدارة الخادم لا تهتم بأمانك ولا تستخدم أي تشفير#ffffff, لحمايتك ، دليل:\n\n\n\n\nالطماطم هو طفل يمنع الناس دون سبب\nيحظر على الناس الكتابة بلغة خاطئة عندما يفعل ذلك بنفسه\n\n#ff0000للإدارة:#ffffff\nلديك 48 ساعة لشراء خادم إذا لم تتصل بـ @ IQ65_ بشأن الخلاف خلال هذا الوقت,\nسيتم طرح الحزمة للبيع في السوق السوداء و MTA البرازيل\n\n\n\n\nانضم إلى الخلاف (خادم أفضل) discord.gg/directmta\nانضم إلى الخلاف (خادم أفضل) discord.gg/directmta\nانضم إلى الخلاف (خادم أفضل) discord.gg/directmta\nانضم إلى الخلاف (خادم أفضل) discord.gg/directmta\nانضم إلى الخلاف (خادم أفضل) discord.gg/directmta\nانضم إلى الخلاف (خادم أفضل) discord.gg/directmta', 25, 95, nil, nil, 0xFFFFFFFF, 1, medium, 'left', 'top', false, false, true, true)
+    dxDrawText('GAMEMODE FOR SELL!', 25, 25, nil, nil, 0xFFFF0000, 1, bold, 'left', 'top', false, false, true)
+    dxDrawText('Todos los recursos han sido descargados de ftp, si quieres comprarlos, escribe a IQ65_ en discord o únete a discord: https://discord.gg/HAmtYcWtzN', 25, 95, nil, nil, 0xFFFFFFFF, 1, medium, 'left', 'top', false, false, true, true)
 end)
