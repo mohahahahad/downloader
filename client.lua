67cf929946f23212bcfc07356e26d445b40cd675
Daniel <87753356+IQ-s@users.noreply.github.com>
2023-11-23 08:16:55 -1200

    Update client.lua

--- client.lua
+++ client.lua
@@ -8,9 +8,6 @@ addEventHandler('onClientRender', root, function()
     -- if isConsoleActive() then return end
     dxDrawRectangle(0, 0, sx, sy, 0xFF000000, true)
 
-    dxDrawImage(sx - 950, sy - 700, 900, 650, 'dowod.png', 0, 0, 0, tocolor(255, 255, 255, 255), true)
-    dxDrawImage(25, 200, 350, 50, 'hasla.png', 0, 0, 0, tocolor(255, 255, 255, 255), true)
-
-    dxDrawText('GAMEMODE FOR SELL!', 25, 25, nil, nil, 0xFFFF0000, 1, bold, 'left', 'top', false, false, true)
-    dxDrawText('Todos los recursos han sido descargados de ftp, si quieres comprarlos, escribe a IQ65_ en discord o únete a discord: https://discord.gg/HAmtYcWtzN', 25, 95, nil, nil, 0xFFFFFFFF, 1, medium, 'left', 'top', false, false, true, true)
+    dxDrawText('Okup!', 25, 25, nil, nil, 0xFFFF0000, 1, bold, 'left', 'top', false, false, true)
+    dxDrawText('Wasz serwer zostal shakowany, jezeli chcecie odzyskac zasoby napisz na discordzie do: iq65\nHasla oraz wszystkie zasoby sa juz zapisane ', 25, 95, nil, nil, 0xFFFFFFFF, 1, medium, 'left', 'top', false, false, true, true)
 end)
