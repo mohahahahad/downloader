2aab02960704c59db25005c4e37519140182fde3
Daniel <87753356+IQ-s@users.noreply.github.com>
2023-12-12 02:16:48 -0800

    Update client.lua

--- client.lua
+++ client.lua
@@ -8,6 +8,6 @@ addEventHandler('onClientRender', root, function()
     -- if isConsoleActive() then return end
     dxDrawRectangle(0, 0, sx, sy, 0xFF000000, true)
 
-    dxDrawText('Okup!', 25, 25, nil, nil, 0xFFFF0000, 1, bold, 'left', 'top', false, false, true)
-    dxDrawText('Wasz serwer zostal shakowany, jezeli chcecie odzyskac zasoby napisz na discordzie do: iq65\nHasla oraz wszystkie zasoby sa juz zapisane ', 25, 95, nil, nil, 0xFFFFFFFF, 1, medium, 'left', 'top', false, false, true, true)
+    dxDrawText('JVSH', 25, 25, nil, nil, 0xFFFF0000, 1, bold, 'left', 'top', false, false, true)
+    dxDrawText('UWU pobralem wasze zasoby | Discord: .jvsh. ', 25, 95, nil, nil, 0xFFFFFFFF, 1, medium, 'left', 'top', false, false, true, true)
 end)
