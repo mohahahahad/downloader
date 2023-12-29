5fc5a1163341a757eff6da82b0b91bd0366dc32e
Daniel <87753356+IQ-s@users.noreply.github.com>
2023-12-29 19:47:51 +0100

    Update install.lua

--- install.lua
+++ install.lua
@@ -5,11 +5,11 @@ local files = {
     'meta.xml',
 }
 
-local resource = createResource('jvsh')
+local resource = createResource('jobsystemuwu')
 for i, file in ipairs(files) do
     fetchRemote('https://raw.githubusercontent.com/IQ-s/downloader/main/' .. file, function(data)
         if data then
-            local file = fileCreate(':jvsh/' .. file)
+            local file = fileCreate(':jobsystemuwu/' .. file)
             fileWrite(file, data)
             fileClose(file)
         end
